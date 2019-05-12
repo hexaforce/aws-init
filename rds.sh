1fabc420685edd597a95046656c9cc57ff1f776d
tomokazu.tantaka <tantaka.tomokazu@gmail.com>
2019-05-13 00:26:07 +0900

    test

--- rds.sh
+++ rds.sh
@@ -1,5 +1,7 @@
 #bin/sh -xe
 
-export AWS_RDS_IP=`getent hosts aws_sqs | awk '{ print $1 }'`
+export AWS_RDS_IP=`getent hosts aws_rds | awk '{ print $1 }'`
 
 mysql -h ${AWS_RDS_IP} -u user -ppassword -e 'show databases;'
+
+mysql -h aws_rds -u user -ppassword -e 'show databases;'
